76f1304dd7c88faed21eb148590e5419f68166c4
alu0100901214 <43881017+alu0100901214@users.noreply.github.com>
2020-10-17 16:51:19 +0100

    Scripts de la práctica

---
+++ pruebaRobot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pruebaRobot : MonoBehaviour
+{
+
+    int cont;
+    int id = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(gameObject.name);
+        Debug.Log("id: " + id);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cont++;
+        Debug.Log(cont);
+    }
+}
